3621a0de9838f80965dd898c98dae4fe4927dc16
kimspark <sir.k.spark@gmail.com>
2015-07-27 08:31:54 +0300

    contract

---
+++ Kadr/Kadr/Data/Entities/Contract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kadr.Data.Common;
+using Kadr.Controllers;
+using System.Data.Linq;
+
+namespace Kadr.Data
+{
+    public partial class Contract : INull, UIX.Views.IDecorable, UIX.Views.IValidatable
+    {
+
+
+        public bool IsNull()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetDecorator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
