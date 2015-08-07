0d6059c55f6e4af5a8127b37173bf955fb7d81f1
kimspark <sir.k.spark@gmail.com>
2015-08-07 17:57:15 +0300

    Education commit

--- Kadr/Kadr/Data/Entities/OK_Educ.cs
+++ Kadr/Kadr/Data/Entities/OK_Educ.cs
@@ -8,18 +8,7 @@ namespace Kadr.Data
 {
     public partial class OK_Educ : UIX.Views.IDecorable, UIX.Views.IValidatable, INull, IComparable
     {
-        public override string ToString()
-        {
-            if ((EducDocumentType == null) || (Spec == null)) return "";
-            try
-            {
-                return EducDocumentType.DocTypeName + " " + Spec;
-            }
-            catch (InvalidOperationException)
-            {
-                return "";
-            }
-        }
+       
 
         #region IDecorable Members
 
