571d6d32b084e8784e0ca9e3c447ff674a8ec0fb
tanvok <tvok@mail.ru>
2015-08-21 14:56:37 +0300

    new

---
+++ Kadr/Kadr/Data/Converters/FinancingSourceConvertor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Kadr.Data.Converters
+{
+    class FinancingSourceConvertor : SimpleToStringConvertor<FinancingSource>// TypeConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        private ICollection GetCollection(System.ComponentModel.ITypeDescriptorContext context)
+        {
+            return Kadr.Controllers.KadrController.Instance.Model.FinancingSources.Where(fs => fs.id < 3).OrderBy(finSource => finSource.FinancingSourceName).ToArray();
+        }
+    }
+}
+
+
