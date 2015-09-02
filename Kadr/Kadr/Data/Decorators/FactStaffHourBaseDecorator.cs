41513ecd5652817b25a7668f2a81ee6b85649e03
tanvok <tvok@mail.ru>
2015-09-02 14:35:17 +0300

    Рефакторинг FactStaffHourBaseDecorator.cs

---
+++ Kadr/Kadr/Data/Decorators/FactStaffHourBaseDecorator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kadr.Data
+{
+    class FactStaffHourBaseDecorator: FactStaffBaseDecorator
+    {
+        public FactStaffHourBaseDecorator(FactStaff factStaff)
+            : base(factStaff)
+        {
+        }
+        
+        public override string ToString()
+        {
+            return "Почасовики (бюджет) отдела " + factStaff.Department.ToString().ToLower();
+        }
+
+        [System.ComponentModel.DisplayName("Количество ставок по нагрузке")]
+        [System.ComponentModel.Category("Почасовики")]
+        [System.ComponentModel.Description("Занимаемое сотрудником количество ставок по нагрузке")]
+        [System.ComponentModel.ReadOnly(true)]
+        public decimal? HourStaffCount
+        {
+            get
+            {
+                return factStaff.HourStaffCount;
+            }
+        }
+
+        [System.ComponentModel.DisplayName("Отдел")]
+        [System.ComponentModel.Category("Общие")]
+        [System.ComponentModel.Description("Название отдела")]
+        [System.ComponentModel.ReadOnly(true)]
+        // [System.ComponentModel.Editor(typeof(Kadr.UI.Editors.PostEditor), typeof(System.Drawing.Design.UITypeEditor))]
+        public Kadr.Data.Dep Department
+        {
+            get
+            {
+                return factStaff.Department;
+            }
+        }
+
+        [System.ComponentModel.DisplayName("Количество часов")]
+        [System.ComponentModel.Category("Почасовики")]
+        [System.ComponentModel.Description("Количество часов для почасовиков")]
+        [System.ComponentModel.ReadOnly(false)]
+        public decimal? HourCount
+        {
+            get
+            {
+                return factStaff.HourCount;
+            }
+            set
+            {
+                factStaff.HourCount = value;
+            }
+        }
+
+        [System.ComponentModel.DisplayName("Оплата за час")]
+        [System.ComponentModel.Category("Почасовики")]
+        [System.ComponentModel.Description("Оплата за час для почасовиков")]
+        [System.ComponentModel.ReadOnly(false)]
+        public decimal? HourSalary
+        {
+            get
+            {
+                return factStaff.HourSalary;
+            }
+            set
+            {
+                factStaff.HourSalary = value;
+            }
+        }
+
+
+        [System.ComponentModel.DisplayName("Вид работы")]
+        [System.ComponentModel.Category("Общие")]
+        [System.ComponentModel.Description("Название вида работы")]
+        [System.ComponentModel.ReadOnly(false)]
+        //[System.ComponentModel.Editor(typeof(Kadr.UI.Editors.WorkTypeEditor), typeof(System.Drawing.Design.UITypeEditor))]
+        public Kadr.Data.WorkType WorkType
+        {
+            get
+            {
+                return factStaff.WorkType;
+            }
+            set
+            {
+                factStaff.WorkType = value;
+            }
+        }
+
+        [System.ComponentModel.DisplayName("Источник финансирования")]
+        [System.ComponentModel.Category("Почасовики")]
+        [System.ComponentModel.Description("Источник финансирования (редактировать для почасовиков)")]
+        [System.ComponentModel.ReadOnly(false)]
+        [System.ComponentModel.TypeConverter(typeof(Kadr.Data.Converters.FinancingSourceConvertor))]
+        public Kadr.Data.FinancingSource FinancingSource
+        {
+            get
+            {
+                return factStaff.FinancingSource;
+            }
+            set
+            {
+                factStaff.FinancingSource = value;
+            }
+        }
+
+        [System.ComponentModel.DisplayName("Комментарий")]
+        [System.ComponentModel.Category("Почасовики")]
+        [System.ComponentModel.Description("Комментарий")]
+        [System.ComponentModel.ReadOnly(false)]
+        public string Comment
+        {
+            get
+            {
+                return factStaff.Comment;
+            }
+            set
+            {
+                factStaff.Comment = value;
+            }
+        }
+
+
+    }
+}
