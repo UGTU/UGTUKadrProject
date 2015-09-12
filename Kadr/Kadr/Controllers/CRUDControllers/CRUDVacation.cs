15026132c41786f44d380b73751a21c8d791027f
sergk <mail2sergk@gmail.com>
2015-09-12 14:29:11 +0300

    Vacations fixed

--- Kadr/Kadr/Controllers/CRUDControllers/CRUDVacation.cs
+++ Kadr/Kadr/Controllers/CRUDControllers/CRUDVacation.cs
@@ -10,7 +10,7 @@ namespace Kadr.Controllers
 {
     public static class CRUDVacation
     {
-        public static void Create(FactStaffHistory fsh, Employee e,BindingSource oKOtpuskBindingSource, object sender)
+        public static void Create(FactStaff fs, Employee e,BindingSource oKOtpuskBindingSource, object sender)
         {
             using (PropertyGridDialogAdding<OK_Otpusk> dlg =
                SimpleActionsProvider.NewSimpleObjectAddingDialog<OK_Otpusk>())
@@ -19,20 +19,20 @@ namespace Kadr.Controllers
                 dlg.InitializeNewObject = (x) =>
                 {
                     dlg.CommandManager.Execute(new UIX.Commands.GenericPropertyCommand<OK_Otpusk, Event>(x, "Event",
-                        new Event(dlg.CommandManager, fsh as FactStaffHistory), null), sender);
+                        new Event(dlg.CommandManager, fs), null), sender);
                     dlg.CommandManager.Execute(new UIX.Commands.GenericPropertyCommand<OK_Otpusk, OK_Otpuskvid>(x, "OK_Otpuskvid", NullOK_Otpuskvid.Instance, null), sender);
                 };
 
                 dlg.ShowDialog();
             }
-            Read(fs, e, oKOtpuskBindingSource);
+            Read(fs.CurrentChange, e, oKOtpuskBindingSource);
         }
 
-        public static void Read(FactStaffHistory fs, Employee e, BindingSource oKOtpuskBindingSource)
+        public static void Read(FactStaffHistory fsh, Employee e, BindingSource oKOtpuskBindingSource)
         {
            IEnumerable<OK_Otpusk> tmp;
 
-            if (fs != null )
+            if (fsh != null )
                 tmp = KadrController.Instance.Model.OK_Otpusks.Where(otp => otp.Event.FactStaffHistory == fsh);
             else
                 tmp = KadrController.Instance.Model.OK_Otpusks.Where(otp => otp.Event.FactStaffHistory.FactStaff.Employee == e);
@@ -48,7 +48,7 @@ namespace Kadr.Controllers
             if (oKOtpuskBindingSource.Current != null)
                 LinqActionsController<OK_Otpusk>.Instance.EditObject(
                         oKOtpuskBindingSource.Current as OK_Otpusk, true);
-            Read(fs, e, oKOtpuskBindingSource);
+            Read(fs.CurrentChange, e, oKOtpuskBindingSource);
         }
 
         public static void Delete(FactStaff fs, Employee e, BindingSource oKOtpuskBindingSource)
@@ -71,7 +71,7 @@ namespace Kadr.Controllers
             KadrController.Instance.Model.OK_Otpusks.DeleteOnSubmit(CurrentOtp);
             LinqActionsController<Event>.Instance.DeleteObject(CurrentPrikaz, KadrController.Instance.Model.Events, null);
 
-            Read(fs, e, oKOtpuskBindingSource);
+            Read(fs.CurrentChange, e, oKOtpuskBindingSource);
         }
     }
 }
