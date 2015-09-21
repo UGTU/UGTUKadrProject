9512df3dfcf14a76f752ef1de84152dac9c937b5
tanvok <tvok@mail.ru>
2015-09-21 11:15:52 +0300

    new

--- Kadr/Kadr/Controllers/MagicNumberController.cs
+++ Kadr/Kadr/Controllers/MagicNumberController.cs
@@ -52,5 +52,22 @@ namespace Kadr.Controllers
                 return Kadr.Controllers.KadrController.Instance.Model.EventKinds.Where(x => x.id == 17).SingleOrDefault();
             }
         }
+
+        static public StandingType DefaultStandingType
+        {
+            get
+            {
+                return KadrController.Instance.Model.StandingTypes.Single(x => x.id == 1);
+            }
+
+        }
+
+        static public RegionType DefaultRegionType
+        {
+            get
+            {
+                return KadrController.Instance.Model.RegionTypes.Single(x => x.id == 1);
+            }
+        }
     }
 }
