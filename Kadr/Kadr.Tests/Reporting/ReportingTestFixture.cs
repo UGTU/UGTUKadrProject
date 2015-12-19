514a8cd9525883988b99c0d8cfbca076c2348e31
Алёна <kurleni@yandex.ru>
2015-12-19 14:38:31 +0300

    -

---
+++ Kadr/Kadr.Tests/Reporting/ReportingTestFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Kadr.Data;
+using System.Diagnostics;
+using Kadr.UI.Reporting;
+
+namespace Kadr.Tests.Reporting
+{
+    [TestClass]
+    public class ReportingTestFixture
+    {
+        [TestMethod]
+        public void CreateScheduleReportTest()
+        {
+            var dataSource = new List<ViewVacationPlan>();
+            for (int i = 0; i < 1500; ++i)
+            {
+                dataSource.Add(new ViewVacationPlan()
+                {
+                    CountDay = 1,
+                    DateBegin = Convert.ToDateTime("2015-08-17 00:00:00.000"),
+                    DateEnd = Convert.ToDateTime("2015-09-29 00:00:00.000"),
+                    LastName = "Курлени",
+                    FirstName = "Алёна",
+                    Otch = "Александровна",
+                    DepartmentName = "Административно-хозяйственное управление",
+                    PostName = "владыка ситхов Дарт Вейдер"
+                });
+            }
+
+            Kadr.Reporting.ScheduleReportBuilder.Create(@"Reporting\template_график.xlsx", @"Reporting\test_schedule_output.xlsx", dataSource, 2016);
+
+            ProcessStartInfo psi = new ProcessStartInfo(@"Reporting\test_schedule_output.xlsx");
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+        }
+
+
+       
+    }
+}
---
+++ Kadr/Kadr/UI/Reporting/ScheduleBuildingScript.cs
@@ -0,0 +1,46 @@
+﻿using Kadr.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kadr.UI.Reporting
+{
+    public class VacationPlanParams
+    {
+        public int Year { get; set; }
+
+        [System.ComponentModel.Browsable(false)]
+        public Guid Department { get; private set; }
+        public string OutputFileName { get; internal set; }
+
+        public VacationPlanParams(Guid departmentId)
+        {
+            var today = DateTime.Today;
+            Year = (today.Month > 8 ? today.Year + 1 : today.Year);
+            Department = departmentId;
+            OutputFileName = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\График отпусков {Year}.xlsx";
+        }
+    }
+    public class ScheduleBuildingScript
+    {
+        public VacationPlanParams VacationParams { get; private set; }
+        public ScheduleBuildingScript(VacationPlanParams vp)
+        {
+            VacationParams = vp;
+        }
+        public void Run()
+        {
+            using (var dlg = new UI.Common.PropertyGridDialog())
+            {
+                dlg.SelectedObjects = new[] { VacationParams };
+                if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    var data = Kadr.Controllers.KadrController.Instance.Model.FetchVacationPlansByDepartmentId(VacationParams.Department, VacationParams.Year);
+
+                    Kadr.Reporting.ScheduleReportBuilder.Create(@"Reporting\template_график.xlsx", VacationParams.OutputFileName, data.AsVacationPlanView(), VacationParams.Year);
+                }
+            }
+        }
+    }
+}
