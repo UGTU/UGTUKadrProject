688828145a9f10104dd5391f6d315afb7c979e94
Алёна <kurleni@yandex.ru>
2015-12-24 17:32:12 +0300

    --

---
+++ Kadr/Kadr/UI/Editors/BrowseFolderEditor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kadr.UI.Editors
+{
+    [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Demand, Name = "FullTrust")]
+    class BrowseFolderEditor : System.Drawing.Design.UITypeEditor
+    {
+
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(FBD.SelectedPath);
+            }
+
+            return FBD.SelectedPath;
+        }
+        
+
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.Modal;
+        }
+
+    }
+}
